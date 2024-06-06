dd475ccd979fa82f2a5475015f1fcc909b118f03
Anishguptaprog <anishajaygupta21@gmail.com>
2024-06-06 08:46:28 +0530

    global DB export

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 // import { Prisma } from "@prisma/client";
-
+//import { Prisma } from "@prisma/client/extension";
 import { PrismaClient } from "@prisma/client";
-
+ 
 const gPrisma = global as unknown as {prisma: PrismaClient};
 
 export const prisma = gPrisma.prisma || new PrismaClient()
