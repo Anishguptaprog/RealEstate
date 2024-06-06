5b1024bb82db97beb44520efd86c14f7f0197bee
Anishguptaprog <anishajaygupta21@gmail.com>
2024-06-06 08:37:01 +0530

    First Commit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+// import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const gPrisma = global as unknown as {prisma: PrismaClient};
+
+export const prisma = gPrisma.prisma || new PrismaClient()
+
+if(process.env.NODE_ENV !== "production") gPrisma.prisma = prisma;
+
+export default prisma;
