16646d38dd7f77fd68dd5ebb1ec457ee3e0014b8
Anishguptaprog <anishajaygupta21@gmail.com>
2024-07-11 11:50:31 +0530

    updated db and property page

--- src/app/user/properties/add/_components/AddPropertyForm.tsx
+++ src/app/user/properties/add/_components/AddPropertyForm.tsx
@@ -1,6 +1,10 @@
 "use client"
 import React, { useState } from 'react'
 import Stepper from './Stepper';
+import { PropertyType, PropertyStatus, Prisma } from '@prisma/client';
+import Basic from './basic';
+import { cn } from '@nextui-org/react';
+import Location from './Location';
 
 const steps = [
     {label:"Basic"},
@@ -10,11 +14,34 @@ const steps = [
     {label:"Contact"},
 ];
 
-const AddPropertyForm = () => {
+interface Props {
+  // className?:string;
+  types: PropertyType[];
+  statuses: PropertyStatus[];
+  // property?: Prisma.PropertyGetPayload<{
+  //   include: {
+  //     location: true;
+  //     contact: true;
+  //     feature: true;
+  //     images: true;
+  //   };
+  // }>;
+  // isEdit?: boolean;
+}
+
+const AddPropertyForm = (props:Props) => {
 
   const [step,setStep] = useState(0);
   return (
-    <div><Stepper items={steps} activeItem={step} setActiveItem={setStep}/></div>
+    <div>
+      <Stepper items={steps} activeItem={step} setActiveItem={setStep}/>
+      <form className='mt-3'>
+        <Basic 
+        className={cn({'hidden':step!=0})}
+        types={props.types} statuses={props.statuses} next={()=> setStep((prev)=>prev+1) }/>  
+        <Location next = {()=>setStep(prev=>prev+1)} prev={()=>setStep(prev=>prev-1)} className={cn({'hidden':step!=1})}/>
+      </form>  
+    </div>
   )
 }
 
--- src/app/user/properties/add/page.tsx
+++ src/app/user/properties/add/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import AddPropertyForm from './_components/AddPropertyForm'
+import prisma from '@/lib/prisma'
 
-const AddPropertyPage = () => {
+const AddPropertyPage = async () => {
+  const [propertyTypes,propertyStatuse] = await Promise.all([
+    prisma.propertyType.findMany(),
+    prisma.propertyStatus.findMany()
+  ])
   return (
-    <AddPropertyForm/>
+    <AddPropertyForm types={propertyTypes} statuses={propertyStatuse}/>
   )
 }
 
